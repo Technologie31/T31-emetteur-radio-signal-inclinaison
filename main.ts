26d3c488f7e005558182c5ef385bd8175f4aedea
Technologie31 <78386526+Technologie31@users.noreply.github.com>
2022-05-13 17:35:53 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 radio.onReceivedNumber(function (receivedNumber) {
     led.plotBarGraph(
     receivedNumber,
-    1024
+    2048
     )
 })
 radio.setGroup(1)
 basic.forever(function () {
-    radio.sendNumber(input.acceleration(Dimension.X))
+    radio.sendNumber(input.acceleration(Dimension.X) + input.acceleration(Dimension.Y))
 })
